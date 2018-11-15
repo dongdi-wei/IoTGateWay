79d12d39df1a20302afd5195788b86fbcdfb70ac
Symvago <45039243+Symvago@users.noreply.github.com>
2018-11-15 11:36:31 +0800

    Update DataGather.go

--- sevice/DataGather.go
+++ sevice/DataGather.go
@@ -49,13 +49,13 @@ func handleConnection(conn net.Conn) {
 }
 
 
-//func Log(v ...interface{}) {
-//	log.Println(v...)
-//}
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
 
 
-//func CheckError(err error) {
-//	if err != nil {
-//		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-//	}
-//}
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	}
+}
