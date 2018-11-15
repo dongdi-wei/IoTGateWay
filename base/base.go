6f3517616457b424e92d584bc4b375c157ef3d86
Symvago <45039243+Symvago@users.noreply.github.com>
2018-11-15 17:05:03 +0800

    2
    2

---
+++ base/base.go
@@ -0,0 +1,12 @@
+package base
+
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
+
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+	}
+}
